27f734a88bceb599627a280b9c4a463eb7f34722
fiko942 <tobellord@gmail.com>
2025-03-15 23:13:59 +0700

    Initialize mobile route display

---
+++ component/Header/MobileRoutes.jsx
@@ -0,0 +1,7 @@
+export default function MobileRoutes({ open, onClose }) {
+    return (
+        <div className="mobile-routes">
+            <div>Menu</div>
+        </div>
+    )
+}
--- component/Header/Navigator.jsx
+++ component/Header/Navigator.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MenuIcon from '@mui/icons-material/Menu';
+import isMobileScreen from "@/utils/isMobileScreen";
+import MobileRoutes from "./MobileRoutes";
 
 export default function Navigator() {
     const [isMobile, setIsMobile] = useState(true)
+    const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
+
+    function handleResize() {
+        setIsMobile(isMobileScreen())
+    }
+
+    useEffect(() => {
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     if(isMobile) { // for the mobile navigator
         return (
-            <div className="flex items-center w-fit ml-auto mr-0 cursor-pointer">
-                <MenuIcon
-                    fontSize="large"
-                    className="text-gray-600"
+            <>
+                <div className="flex items-center w-fit ml-auto mr-0 cursor-pointer"
+                    onClick={() => { setIsMobileNavOpen(true) }}
+                >
+                    <MenuIcon
+                        fontSize="large"
+                        className="text-gray-600"
+                    />
+                </div>
+                <MobileRoutes
+                    open={isMobileNavOpen}
+                    onClose={() => setIsMobileNavOpen(false)}
                 />
-            </div>
-        )
+            </>
+        )   
     }
 
     // For the desktop navigator
